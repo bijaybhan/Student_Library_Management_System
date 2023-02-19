cd275b749790ef56ad40d80e8ab66065cd4d4de7
Bijaybhan Gond <bijaybhan92@gmail.com>
2023-02-20 00:22:05 +0530

    MajorProject

---
+++ src/main/java/com/example/Student_Library_Management_System/Enums/CardStatus.java
@@ -0,0 +1,14 @@
+package com.example.Student_Library_Management_System.Enums;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public enum CardStatus
+{
+    ACTIVATED,
+    DEACTIVATED,
+    BLOCKED,
+    EXPIRED
+
+}
---
+++ src/main/java/com/example/Student_Library_Management_System/Services/CardService.java
@@ -0,0 +1,9 @@
+package com.example.Student_Library_Management_System.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService
+{
+
+}
